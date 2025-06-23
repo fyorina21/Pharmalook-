fdf4eae969db7b2b24a6ed763b4be70c1b1ab6b7
fyorina21 <fyorinegasiabriha@gmail.com>
2025-06-23 10:32:43 +0300

    updated

--- app/auth.py
+++ app/auth.py
@@ -42,21 +42,21 @@ def login():
 
 
 @auth_bp.route("/psignup", methods=["GET", "POST"])
-def signup():
+def psignup():
     if request.method == "POST":
         name = request.form.get("name")
-        location = request.form.get("lastname")
+        location = request.form.get("loaction")
         email = request.form.get("email")
         password = request.form.get("password")
 
         user = Users.query.filter_by(email=email).first()
-        if user:
+        if pharmacist:
             print("User exists with this email. Try logging in.", "error")
             return redirect(url_for("auth_bp.signup"))
 
-        new_user = Users(name=name,location=location, email=email)
-        new_user.set_password(password)
-        db.session.add(new_user)
+        pharmacist = Users(name=name,location=location, email=email)
+        user.set_password(password)
+        db.session.add(pharmacist)
         db.session.commit()
 
 
@@ -75,7 +75,38 @@ def signup():
     return render_template("psignup.html")
 
 
+@auth_bp.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        name = request.form.get("name")
+        lastname = request.form.get("lastname")
+        email = request.form.get("email")
+        password = request.form.get("password")
 
+        user = Users.query.filter_by(email=email).first()
+        if user:
+            print("User exists with this email. Try logging in.", "error")
+            return redirect(url_for("auth_bp.signup"))
+
+        new_user = Users(name=name,lastname=lastname, email=email)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+
+
+        if user:
+            session['id'] = user.id
+            session['username'] = user.name
+            flash("Registered successfully", "success")
+            # maybe redirect or return success
+            
+        else:
+            flash("User not found. Please check your email or sign up.")
+            return redirect(url_for("auth_bp.signup"))  # or wherever your signup route is
+
+        return redirect(url_for("routes_bp.search"))
+    print("error")
+    return render_template("signup.html")
 
 
 @auth_bp.route("/logout")
